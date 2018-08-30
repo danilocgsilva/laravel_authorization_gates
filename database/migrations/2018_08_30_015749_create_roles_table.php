8fcf53886f1c98bc14c018ca941792fe5693ee79
Danilo Silva <danilocgsilva@gmail.com>
2018-08-29 22:59:20 -0300

    Migrations and some models created

--- database/migrations/2018_08_30_015749_create_roles_table.php
+++ database/migrations/2018_08_30_015749_create_roles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostsTable extends Migration
+class CreateRolesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,12 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title');
+            $table->string('name');
             $table->string('slug')->unique();
-            $table->text('body');
-            $table->boolean('published')->default(false);
-            $table->unsignedInteger('user_id');
+            $table->jsonb('permissions')->default('{}'); // jsonb deletes duplicates
             $table->timestamps();
-        
-            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
@@ -33,6 +29,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('roles');
     }
 }
